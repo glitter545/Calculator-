baaf3c5f9d6037f3667b20b0d469d0e7043a8340
glitter545 <bobbybroken992@gmail.com>
2023-02-13 15:48:41 -0800

    Implemented the capability to perform calculations with multiple numbers

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const display = document.querySelector('.display p');
 let operator = '';
 let firstValue = '';
 let secondValue = '';
+let thirdValue = '';
 
 buttons.forEach(button => {
   button.addEventListener('click', e => {
@@ -13,11 +14,40 @@ buttons.forEach(button => {
       display.innerText = '';
       firstValue = '';
       secondValue = '';
+      thirdValue = '';
       operator = '';
     } else if (value === '+' || value === '-' || value === 'x' || value === '/') {
-      operator = value;
-      firstValue = parseFloat(display.innerText);
-      display.innerText = '';
+      if (!operator) {
+        operator = value;
+        firstValue = parseFloat(display.innerText);
+        display.innerText = '';
+      } else {
+        secondValue = parseFloat(display.innerText);
+        switch (operator) {
+          case '+':
+            thirdValue = firstValue + secondValue;
+            break;
+          case '-':
+            thirdValue = firstValue - secondValue;
+            break;
+          case 'x':
+            thirdValue = firstValue * secondValue;
+            break;
+          case '/':
+            if (secondValue === 0) {
+              display.innerText = 'Error';
+            } else {
+              thirdValue = firstValue / secondValue;
+            }
+            break;
+          default:
+            display.innerText = 'Error';
+        }
+        operator = value;
+        firstValue = thirdValue;
+        display.innerText = '';
+        secondValue = '';
+      }
     } else if (value === '=') {
       secondValue = parseFloat(display.innerText);
       switch (operator) {
